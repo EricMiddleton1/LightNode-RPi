0eecc2b7982b867e664d46ae6071efba799c44e6
Eric Middleton <ericm@iastate.edu>
2017-11-18 23:25:20 -0600

    Minor updates to LightLB130. Updated to latest LightNode

--- src/LightLB130.cpp
+++ src/LightLB130.cpp
@@ -7,44 +7,39 @@
 LightLB130::LightLB130(boost::asio::io_service& ioService, const std::string& _name,
 	const std::string& _ipAddr, uint16_t _port)
 	:	Light(ioService, _name, 1)
-	,	ioWork(std::make_unique<boost::asio::io_service::work>(ioService))
 	,	endpoint(boost::asio::ip::address::from_string(_ipAddr), _port)
 	,	socket(ioService, boost::asio::ip::udp::v4())
-	,	asyncThread(std::bind(&LightLB130::threadRoutine, this)) {
-}
-
-LightLB130::~LightLB130() {
-	ioWork.reset();
-	ioService.stop();
-
-	asyncThread.join();
-}
-
-void LightLB130::threadRoutine() {
-	ioService.run();
-
-	std::cout << "LightLB130::threadRoutine: Exiting..." << std::endl;
+	,	tick{0} {
 }
 
 void LightLB130::update() {
-	auto ledColor = leds[0].getColor();
+	if(tick == 0) {
+		auto ledColor = leds[0].getColor();
 
-	double r = std::pow(ledColor.getRed()/255., 2.2),
-		g = std::pow(ledColor.getGreen()/255., 2.2),
-		b = std::pow(ledColor.getBlue()/255., 2.2);
-									
-	Color c(255.*r + 0.5, 255.*g + 0.5, 255.*b + 0.5);
+		double r = std::pow(ledColor.getRed()/255., 2.2),
+			g = std::pow(ledColor.getGreen()/255., 2.2),
+			b = std::pow(ledColor.getBlue()/255., 2.2);
+										
+		Color c(255.*r + 0.5, 255.*g + 0.5, 255.*b + 0.5);
 
-	auto hue = c.getHue(), sat = c.getSat(), val = c.getVal();
+		auto hue = c.getHue(), sat = c.getSat(), val = c.getVal();
 
-	std::string command = "{\"smartlife.iot.smartbulb.lightingservice\""
-		":{\"transition_light_state\":{\"ignore_default\":1,\"on_off\":" + 
-		std::to_string(leds[0].isTargetOn() && (val > 0)) + ",\"color_temp\":0,"
-		"\"hue\":" + std::to_string((int)(360.f*hue/256.f)) + ",\"saturation\":" +
-		std::to_string((int)(100.f*sat/256.f)) + ",\"brightness\":" +
-		std::to_string((int)(100.f*val/256.f)) + ",\"transition_period\":20}}}";
-	
-	socket.send_to(boost::asio::buffer(encrypt(command)), endpoint);
+		std::string command = "{\"smartlife.iot.smartbulb.lightingservice\""
+			":{\"transition_light_state\":{\"ignore_default\":1,\"on_off\":" + 
+			std::to_string(leds[0].isTargetOn() && (val > 0)) + ",\"color_temp\":0,"
+			"\"hue\":" + std::to_string((int)(360.f*hue/256.f)) + ",\"saturation\":" +
+			std::to_string((int)(100.f*sat/256.f)) + ",\"brightness\":" +
+			std::to_string((int)(100.f*val/256.f)) + ",\"transition_period\":20}}}";
+		
+		try {
+			socket.send_to(boost::asio::buffer(encrypt(command)), endpoint);
+		}
+		catch(const std::exception& e) {
+			std::cerr << "[Error] LightLB130::update: " << e.what() << std::endl;
+		}
+	}
+
+	tick = (tick + 1) % 20;
 }
 
 std::vector<uint8_t> LightLB130::encrypt(const std::string& command) {
--- src/LightLB130.hpp
+++ src/LightLB130.hpp
@@ -13,21 +13,16 @@
 class LightLB130 : public Light
 {
 public:
-	LightLB130(boost::asio::io_service& ioService, const std::string& name, const std::string& ipAddr, uint16_t port);
-	virtual ~LightLB130();
+	LightLB130(boost::asio::io_service& ioService, const std::string& name,
+		const std::string& ipAddr, uint16_t port);
 
 private:
 	virtual void update() override;
 	
-	void threadRoutine();
-	
 	static std::vector<uint8_t> encrypt(const std::string& command);
 
-	boost::asio::io_service ioService;
-	std::unique_ptr<boost::asio::io_service::work> ioWork;
-	
 	boost::asio::ip::udp::endpoint endpoint;
 	boost::asio::ip::udp::socket socket;
 
-	std::thread asyncThread;
+	unsigned int tick;
 };
